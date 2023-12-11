3a51e4a719dc0365a057b06a800edfa5f2d6349c
Andrew <ondralukes06@seznam.cz>
2023-12-11 07:28:58 +0100

    styled the discord invite btuton

--- app/canvasEditor/page.tsx
+++ app/canvasEditor/page.tsx
@@ -27,7 +27,7 @@ import { theme } from "./theme/theme";
 import { ThemeProvider, createTheme} from "@mui/material"
 import DiscordInviteBtn from "../components/global/DiscordInviteBtn";
 // import { MousePositionContext, MousePositionProvider } from "./MouseContext";
-
+import {InlineFollowButtons} from 'sharethis-reactjs';
 
 const Page: React.FC = () => {
   const {   canvasRef, frontlineCanvasRef, markerCanvasRef,backgroundCanvasRef } =
@@ -103,7 +103,34 @@ const Page: React.FC = () => {
       />
       <CanvasSettings />
       <CanvasEditor />
-       <DiscordInviteBtn />
+      {/* <InlineFollowButtons
+        //   config={{
+        //     action: 'Follow us:', // call to action (STRING)
+        //     action_enable: true,  // show/hide call to action (true, false)
+        //     action_pos: 'bottom', // position of call to action (left, top, right)
+        //     alignment: 'center',  // alignment of buttons (left, center, right)
+        //     color: 'white',       // set the color of buttons (social, white)
+        //     enabled: true,        // show/hide buttons (true, false)
+        //     networks: [           // which networks to include (see FOLLOW NETWORKS)
+        //       'twitter',
+        //       'facebook',
+        //       'instagram',
+        //       'youtube'
+        //     ],
+        //     padding: 8,           // padding within buttons (INTEGER)
+        //     profiles: {
+        //       // discord:  'https://discord.com/api/oauth2/authorize?client_id=1183464624717312134&permissions=2165312&scope=bot',       // social profile links for buttons
+        //       twitter: 'sharethis',
+        //       facebook: 'sharethis',
+        //       instagram: 'sharethis',
+        //       youtube: '/channel/UCbM93niCmdc2RD9RZbLMP6A?view_as=subscriber'
+        //     },
+        //     radius: 9,            // the corner radius on each button (INTEGER)
+        //     size: 32,             // the size of each button (INTEGER)
+        //     spacing: 8            // the spacing between buttons (INTEGER)
+        //   }}
+          />*/}
+         <DiscordInviteBtn />
       <Timeline />
     </>
   );
--- app/components/global/DiscordInviteBtn.tsx
+++ app/components/global/DiscordInviteBtn.tsx
@@ -1,18 +1,30 @@
-// In your React component or Next.js page
-import React from 'react';
+import React, { useState } from 'react';
+import Image from 'next/image';
 
 const DiscordInviteBtn = () => {
   // Your Discord invite link
   const discordInviteLink = 'https://discord.com/api/oauth2/authorize?client_id=1183464624717312134&permissions=2165312&scope=bot';
 
+  const [isInviteVisible, setInviteVisible] = useState(true);
+
+  const handleHideInvite = () => {
+    setInviteVisible(false);
+  };
+
   return (
-    <div className='text-black'>
-      {/* Your other content */}
-      <p>Join our Discord server:</p>
-      <a href={discordInviteLink} target="_blank" rel="noopener noreferrer">
-        <img src="/discord-icon.png" alt="Discord Icon" />
-        Join Discord
-      </a>
+    <div>
+      {isInviteVisible && (
+        <button className='  select-none relative flex items-center text-white py-2 px-4 rounded' style={{ backgroundColor: '#5865f2' }}>
+          {/* Your other content */}
+          <p className="mr-2">Join Discord</p>
+          <a className='flex items-center' href={discordInviteLink} target="_blank" rel="noopener noreferrer">
+            <Image src="/discord.png" alt="Discord Icon" width={32} height={32} priority={true} />
+          </a>
+          <button className='absolute -top-2 -right-2 bg-red-600   p-1 'onClick={handleHideInvite}>X</button>
+        </button>
+      )}
+
+
     </div>
   );
 };
